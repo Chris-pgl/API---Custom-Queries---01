47729f74169926df40f5e722e33c20ed93948fa2
Chris <cr1s@hotmail.it>
2023-04-01 19:20:14 +0200

    Aggiunto service per avere il codice più pulito

--- src/main/java/com/example/costumQuery1/controller/FlightController.java
+++ src/main/java/com/example/costumQuery1/controller/FlightController.java
@@ -3,6 +3,7 @@ package com.example.costumQuery1.controller;
 import com.example.costumQuery1.entity.Flight;
 import com.example.costumQuery1.entity.FlightStatus;
 import com.example.costumQuery1.repository.FlightRepository;
+import com.example.costumQuery1.service.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,39 +20,17 @@ import java.util.Random;
 public class FlightController {
 
     @Autowired
-    private FlightRepository flightRepository;
-
-    public String generetedString(){
-        int leftLimit = 48; // numeral '0'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 8;
-        Random random = new Random();
-
-        String generatedString = random.ints(leftLimit, rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                .limit(targetStringLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-
-        return generatedString;
-    }
+    private FlightService flightService;
 
     @PostMapping("/generate")
     public ResponseEntity genereteFlight(){
-        for(int i = 0; i < 50; i++){
-            Flight flight = new Flight();
-            flight.setDescription(generetedString());
-            flight.setFromAirport(generetedString());
-            flight.setToAirport(generetedString());
-            flight.setStatus(FlightStatus.ON_TIME);
-            flightRepository.save(flight);
-        }
+        flightService.genereteFlight();
         return ResponseEntity.ok("created 50 flights");
     }
 
     @GetMapping("/get-flights")
     public ResponseEntity getAllFlight(){
-        return ResponseEntity.ok(flightRepository.findAll());
+        return ResponseEntity.ok(flightService.getAllFlights());
     }
 
 
